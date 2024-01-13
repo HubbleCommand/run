b4d7753c491beb7d4ad98806ce494a6ce261ad54
HubbleCommand <sasha.poirier@gmail.com>
2024-01-13 18:05:27 +0100

    better runner with process

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,13 @@
 import * as vscode from 'vscode';
 
+function buildStartCommand(path: string): string {
+	var command: string = path;
+	if (process.platform === 'win32') {
+		command = "start " + command;
+	}
+	return command;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	let disposableOpen = vscode.commands.registerCommand('run.open', (file) => {
@@ -7,6 +15,24 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	
 	context.subscriptions.push(disposableOpen);
+
+	let disposableRun = vscode.commands.registerCommand('run.run', (file) => {
+		if (file.scheme !== "file") {
+			console.log("Not a file, quitting");
+			return;
+		}
+		
+		const cp = require('child_process');
+		cp.exec(buildStartCommand(file.fsPath), (err :Error, stdout :string|Buffer, stderr :string|Buffer) => {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (err) {
+				console.log('error: ' + err);
+			}
+		});
+	});
+
+	context.subscriptions.push(disposableRun);
 }
 
 export function deactivate() {}
